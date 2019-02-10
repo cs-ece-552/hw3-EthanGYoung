7e93c086ac691b0cec10386146b6886d49e6cf63
Ethan Young <eyoung@rockhopper-04.cs.wisc.edu>
2019-02-10 15:00:31 -0600

    Shortened const 0 to 1 bit instead of 32 bits

--- hw3_2/rca_16b.v
+++ hw3_2/rca_16b.v
@@ -15,7 +15,7 @@ module rca_16b(A, B, S, C_out);
 
     wire c0, c1, c2;
 
-     rca_4b bits_0_3(A[3:0], B[3:0], 0, S[3:0], c0); 
+     rca_4b bits_0_3(A[3:0], B[3:0], 1'b0, S[3:0], c0); 
      rca_4b bits_4_7(A[7:4], B[7:4], c0, S[7:4], c1); 
      rca_4b bits_8_11(A[11:8], B[11:8], c1, S[11:8], c2); 
      rca_4b bits_12_15(A[15:12], B[15:12], c2, S[15:12], C_out); 
