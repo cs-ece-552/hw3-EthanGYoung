3018df927f1799ee6405d508d477bcd75a7c9ce2
Ethan Young <eyoung@rockhopper-04.cs.wisc.edu>
2019-02-10 14:44:01 -0600

    Implemented part 1 of second problem. Untested.

--- hw3_2/fullAdder_1b.v
+++ hw3_2/fullAdder_1b.v
@@ -6,10 +6,22 @@
 */
 module fullAdder_1b(A, B, C_in, S, C_out);
     input  A, B;
-	input  C_in;
+    input  C_in;
     output S;
     output C_out;
 
-	// YOUR CODE HERE
+    wire s0, s1, c0, c1, c2;
+   
+    xor3
+    	x0(s0, A, B, C_in);
+    nor3
+    	n0(s1, A, B, C_in);
+    nand2
+    	a0(S, s0, s1),
+	a1(c0, A,B),
+	a2(c1,A,C_in),
+	a3(c2,B,C_in);
+    nand3
+    	a4(C_out, c0, c1, c2);
 
 endmodule
