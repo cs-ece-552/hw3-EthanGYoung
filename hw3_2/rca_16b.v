e0ca295f8ec70a89e473cc406c6fd0af70a042cd
Ethan Young <eyoung@rockhopper-04.cs.wisc.edu>
2019-02-10 14:55:45 -0600

    Implemented ripple carry adder. Untested

--- hw3_2/rca_16b.v
+++ hw3_2/rca_16b.v
@@ -13,6 +13,11 @@ module rca_16b(A, B, S, C_out);
     output [N-1:0] S;
     output         C_out;
 
-	// YOUR CODE HERE
+    wire c0, c1, c2;
+
+     rca_4b bits_0_3(A[3:0], B[3:0], 0, S[3:0], c0); 
+     rca_4b bits_4_7(A[7:4], B[7:4], c0, S[7:4], c1); 
+     rca_4b bits_8_11(A[11:8], B[11:8], c1, S[11:8], c2); 
+     rca_4b bits_12_15(A[15:12], B[15:12], c2, S[15:12], C_out); 
 
 endmodule
